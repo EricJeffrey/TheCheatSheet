a4893dd43b5058798482490fbd0918bc38fc9ce5
EricJeffrey <1719937412@qq.com>
2020-10-20 22:14:35 +0800

    init back end & front end code folder

---
+++ backend/src/test/java/com/thecheatsheet/CheatSheet/CheatSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecheatsheet.CheatSheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheatSheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
