60cc210e63f4ae4d139714652b8931b77924ef71
EricJeffrey <1719937412@qq.com>
2020-11-01 21:26:31 +0800

    make database & es data mounted by volumes

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,9 +3,9 @@
 # build frontend -> cp to backend -> build backend -> cp to deploy/ -> run docker((podman -t hostnet) for fedora)-compose up 
 
 echo '停止服务'
-sudo docker-compose down --remove-orphans
+sudo docker-compose -p cheatsheet down --remove-orphans
 
-echo '请确保https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v7.9.2/elasticsearch-analysis-ik-7.9.2.zip已经下载到当前文件夹，任意键继续...'
+echo '请确保 https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v7.9.2/elasticsearch-analysis-ik-7.9.2.zip 已经下载到当前文件夹，任意键继续...'
 
 a=2
 read a
@@ -15,6 +15,10 @@ set -e # exit on any error
 set -x # show command
 
 cd ../
+
+echo '创建数据库目录(若存在则忽略)'
+sudo mkdir -m 775 -p /data/cheatsheet/db_mysql
+sudo mkdir -m 775 -p /data/cheatsheet/elasticsearch
 
 echo '开始打包前端'
 cd frontend/thecheatsheet/ && npm install && npm run build && cd ../../
@@ -30,8 +34,7 @@ cp -f backend/target/CheatSheet-1.0.0.war deploy/cheatsheet.war
 
 echo '启动服务'
 # force recreate container and rebuild image
-# note this will !!! CLEAN !!! both mysql and elasticsearch, might be better to mount their data as volume in dockerfile
-cd deploy && sudo docker-compose up -d --force-recreate --build
+cd deploy && sudo docker-compose -p cheatsheet -f docker-compose.yml up --force-recreate --build --remove-orphans -d
 #cd deploy && sudo podman-compose -t hostnet up -d --force-recreate --build
 
 
