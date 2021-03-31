5f83e699cbba444ee49cfd3698f7d03c70fc9de0
EricJeffrey <1719937412@qq.com>
2021-03-31 09:41:38 +0800

    conan and mongocxx driver

--- backend/main.cc
+++ backend/main.cc
@@ -1,11 +1,45 @@
 #include "lib/httplib.h"
-#include <Windows.h>
+#include <bsoncxx/json.hpp>
+#include <bsoncxx/builder/stream/array.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/builder/stream/helpers.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
 
-int main(int argc, char const *argv[]) {
+void mongoTest() {
+    mongocxx::instance instance{};
+    mongocxx::uri tmpUri("mongodb://172.17.0.2:27017");
+    mongocxx::client client(tmpUri);
+    auto db = client.database("cheatsheet");
+    auto builder = bsoncxx::builder::stream::document{};
+    bsoncxx::document::value doc_value =
+        builder << "name"
+                << "MongoDB"
+                << "type"
+                << "database"
+                << "count" << 1 << "versions" << bsoncxx::builder::stream::open_array << "v3.2"
+                << "v3.0"
+                << "v2.6" << bsoncxx::builder::stream::close_array << "info"
+                << bsoncxx::builder::stream::open_document << "x" << 203 << "y" << 102
+                << bsoncxx::builder::stream::close_document << bsoncxx::builder::stream::finalize;
+    auto collection = db.collection("test");
+    collection.insert_one(doc_value.view());
+    mongocxx::cursor cursor = collection.find({});
+    for (auto doc : cursor) {
+        std::cout << bsoncxx::to_json(doc) << "\n";
+    }
+}
+
+void serverTest() {
     httplib::Server server;
-    server.Get("/hi", [&](const httplib::Request &request, httplib::Response &response) {
+    server.Get("/", [&](const httplib::Request &request, httplib::Response &response) {
         response.set_content("hello world", "text/plain");
     });
     server.listen("0.0.0.0", 8000);
+}
+
+int main(int argc, char const *argv[]) {
+    mongoTest();
+    // serverTest();
     return 0;
 }
